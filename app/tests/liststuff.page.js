90cb54e9192f27eed2c0ab342dad1d7fefe70d62
Cory-Mac <school.coryparker@gmail.com>
2022-04-17 21:56:24 -1000

    increase wait time for table

--- app/tests/liststuff.page.js
+++ app/tests/liststuff.page.js
@@ -15,7 +15,7 @@ class ListStuffPage {
   async hasTable(testController) {
     // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
     const rowCount = Selector('tr').count; // tr = <tr>  .tr = class='tr' #tr = id='hr'
-    await testController.expect(rowCount).gte(2);
+    await testController.wait(60000).expect(rowCount).gte(2);
   }
 }
 
